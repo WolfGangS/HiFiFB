5ef864e994ec18254bc41851e0de5bb27a043397
WolfGang <flamin2k8@gmail.com>
2018-04-13 23:33:29 +0100

    php

--- index.php
+++ index.php
@@ -21,6 +21,48 @@ function dump($var) {
 	echo "</pre>";
 }
 
+function scan($dir) {
+
+	$files = array();
+
+	// Is there actually such a folder/file?
+
+	if (file_exists($dir)) {
+
+		foreach (scandir($dir) as $f) {
+
+			if (!$f || $f[0] == '.') {
+				continue; // Ignore hidden files
+			}
+
+			if (is_dir($dir . '/' . $f)) {
+
+				// The path is a folder
+
+				$files[] = array(
+					"name" => $f,
+					"type" => "folder",
+					"path" => $dir . '/' . $f,
+					"items" => scan($dir . '/' . $f), // Recursively get the contents of the folder
+				);
+			} else {
+
+				// It is a file
+
+				$files[] = array(
+					"name" => $f,
+					"type" => "file",
+					"path" => $dir . '/' . $f,
+					"size" => filesize($dir . '/' . $f), // Gets the size of this file
+				);
+			}
+		}
+
+	}
+
+	return $files;
+}
+
 $uri = [];
 $_uri = trim(trim($_SERVER["REQUEST_URI"]), "\\/");
 if (strlen($_uri) > 0) {
@@ -30,14 +72,43 @@ if (strlen($_uri) > 0) {
 			$uri[] = $ur;
 		}
 	}
+} else {
+	$uri[] = "";
 }
 
 dump($uri);
-die();
+
+$readfile = null;
+
+$headers = [];
+$jsonResponse = [];
 
 switch ($_SERVER["REQUEST_METHOD"]) {
 case "GET":
-	//if()
+	switch (strtolower($uri[0])) {
+	case "hififb.js";
+		echo "HIFIFB";
+		break;
+	case "json";
+		$jsonResponse = [
+			"name" => "files",
+			"type" => "folder",
+			"path" => "Assets",
+			"items" => scan("Assets"),
+		];
+		break;
+	case "assets":
+		echo "assets 4 u";
+		break;
+	case "html":
+		if (is_file("index.html")) {
+			$readfile = "index.html";
+		}
+		break;
+	default:
+		http_response_code(404);
+		break;
+	}
 	break;
 case "POST":
 
@@ -45,4 +116,16 @@ case "POST":
 default:
 
 	break;
+}
+
+if (!empty($jsonResponse)) {
+	header('Content-Type: application/json');
+	echo json_encode($jsonResponse);
+} else {
+	foreach ($headers as $header) {
+		header($header);
+	}
+	if ($readfile !== null) {
+		readfile($readfile);
+	}
 }
