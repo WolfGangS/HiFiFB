1deb871850c061e11a64aefc7c88840af6ca76eb
WolfGang <flamin2k8@gmail.com>
2018-04-13 23:00:46 +0100

    dump

--- app/js/script.js
+++ app/js/script.js
@@ -6,7 +6,7 @@ $(function(){
 
 	// Start by fetching the file data from scan.php with an AJAX request
 
-	$.get('scan.php', function(data) {
+	$.get("/get-assets", function(data) {
 
 		var response = [data],
 			currentPath = '',
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * jQuery File Upload Plugin PHP Example
+ * https://github.com/blueimp/jQuery-File-Upload
+ *
+ * Copyright 2010, Sebastian Tschan
+ * https://blueimp.net
+ *
+ * Licensed under the MIT license:
+ * https://opensource.org/licenses/MIT
+ */
+/*
+error_reporting(E_ALL | E_STRICT);
+require('UploadHandler.php');
+$upload_handler = new UploadHandler();
+
+ */
+echo "<pre>";
+print_r($_REQUEST);
+echo "</pre>";
+
+echo "<hr>";
+
+echo "<pre>";
+print_r($_SERVER);
+echo "</pre>";
