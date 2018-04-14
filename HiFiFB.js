0937713c5848f6d097c94e1a8df52364c8d5b939
WolfGang <flamin2k8@gmail.com>
2018-04-14 12:37:17 +0100

    php

--- HiFiFB.js
+++ HiFiFB.js
@@ -1,6 +1,97 @@
+//Deine out Scope
+(function() {
+  
+  //////////////////////////////
+  /////////// Overlay //////////
+  //////////////////////////////
+  
+  var overlayHTML = "fb.html";
+  var overlayName = "File Browser";
+  
+  var overlayProps = {
+    title: overlayName,  
+    source: Script.resolvePath(overlayHTML), 
+    width: 400, 
+    height: 800, 
+    visible: true, 
+    pinnable: false,
+  };
+  
+  var webOverlay = new OverlayWebWindow(overlayProps);
 
-(function(){
+  webOverlay.webEventReceived.connect(webEvent);
+  function webEvent(msg){
+    switch(msg.type)
+  }
+  
+  function sendScriptEvent(type,data){
+    data = JSON.stringify({ type: type, data: data });
+    webOverlay.emitScriptEvent(data);
+  }
+  
 
-    console.log("RAWR");
+  function toggleWebOverlay(){
+    setWebOverlay(!webOverlay.isVisible());
+  }
 
+  function openWebOverlay(){
+    setWebOverlay(true);
+  }
+
+  function closeOverlay(){
+    setWebOverlay(false);
+  }
+  
+  function setWebOverlay(state){
+    webOverlay.setVisible(state);
+    button.editProperties({ isActive: state });
+  }
+  
+  
+  
+  //////////////////////////////
+  /////////// Button ///////////
+  //////////////////////////////
+  
+  var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+  
+  var button = tablet.addButton({
+    icon: Script.resolvePath("web-icon-white.svg"),
+    activeIcon: Script.resolvePath("web-icon-black.svg"),
+    text: "Web Example",
+    isActive: false,
+    sortOrder: 30
+  });
+
+  button.clicked.connect(toggleWebOverlay);
+  
+  
+  
+  //////////////////////////////
+  //////// Do Something ////////
+  //////////////////////////////
+  
+  //Lets send something to our overlay to show that the connection works,
+  //and as an example of how you can use the connection
+  //Script.setInterval works like window.setInterval in a browser, it starts a timer that will call the function inside every so many miliseconds
+  /*
+  Script.setInterval(function(){
+    //send a script event with the function we made earlier, setting a type so that we can tell in the overlay what to do with it
+    sendScriptEvent("avatar-position",MyAvatar.position);
+  },2000);//2000 will result in a call every 2 seconds
+  */
+  
+  
+  //////////////////////////////
+  ////////// Cleanup ///////////
+  //////////////////////////////
+  
+  //Connect to the scriptEnding event to cleanup our objects
+  Script.scriptEnding.connect(scriptEnding);
+  function scriptEnding(){
+    button.clicked.disconnect(toggleWebOverlay);
+    tablet.removeButton(button);
+    webOverlay.webEventReceived.disconnect(webEvent);
+    webOverlay.close();
+  }
 })();
