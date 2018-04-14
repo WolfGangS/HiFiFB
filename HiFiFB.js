c07ad5778821b3a12ed77c23e247a690889f5e51
WolfGang <flamin2k8@gmail.com>
2018-04-14 14:08:38 +0100

    php

--- HiFiFB.js
+++ HiFiFB.js
@@ -1,88 +1,96 @@
 //Deine out Scope
 var DEV_MODE = true;
 (function() {
-  
-  //////////////////////////////
-  /////////// Overlay //////////
-  //////////////////////////////
-  
-  var DEV_STRING = DEV_MODE ? "?dev=" + Date.now() : "";
 
-  var overlayHTML = "fb.html" + DEV_STRING;
-  var overlayName = "File Browser";
-  
-  var overlayProps = {
-    title: overlayName,  
-    source: Script.resolvePath(overlayHTML), 
-    width: 400, 
-    height: 800, 
-    visible: true, 
-    pinnable: false,
-  };
-  
-  var webOverlay = new OverlayWebWindow(overlayProps);
+    //////////////////////////////
+    /////////// Overlay //////////
+    //////////////////////////////
 
-  webOverlay.webEventReceived.connect(webEvent);
-  function webEvent(msg){
-    switch(msg.type){
-        case "log":
-            console.log("FROM WEB: ",msg);
-            break;
+    var DEV_STRING = DEV_MODE ? "?dev=" + Date.now() : "";
+
+    var overlayHTML = "fb.html" + DEV_STRING;
+    var overlayName = "File Browser";
+
+    var overlayProps = {
+        title: overlayName,
+        source: Script.resolvePath(overlayHTML),
+        width: 400,
+        height: 800,
+        visible: true,
+        pinnable: false,
+    };
+
+    var webOverlay = new OverlayWebWindow(overlayProps);
+
+    webOverlay.webEventReceived.connect(webEvent);
+
+    function webEvent(msgs) {
+        if (!Array.isArray(msgs)) {
+            msgs = [msgs];
+        }
+        for (var i = 0; i < msgs.length; i++) {
+            var msg = msgs[i];
+            switch (msg.type) {
+                case "log":
+                    console.log("FROM WEB: ", msg);
+                    break;
+            }
+        }
     }
-  }
-  
-  function sendScriptEvent(type,data){
-    data = JSON.stringify({ type: type, data: data });
-    webOverlay.emitScriptEvent(data);
-  }
-  
 
-  function toggleWebOverlay(){
-    setWebOverlay(!webOverlay.isVisible());
-  }
+    function sendScriptEvent(type, data) {
+        data = JSON.stringify({ type: type, data: data });
+        webOverlay.emitScriptEvent(data);
+    }
 
-  function openWebOverlay(){
-    setWebOverlay(true);
-  }
 
-  function closeOverlay(){
-    setWebOverlay(false);
-  }
-  
-  function setWebOverlay(state){
-    webOverlay.setVisible(state);
-    button.editProperties({ isActive: state });
-  }
-  
-  
-  
-  //////////////////////////////
-  /////////// Button ///////////
-  //////////////////////////////
-  
-  var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
-  
-  var button = tablet.addButton({
-    icon: Script.resolvePath("folder-white.svg"),
-    activeIcon: Script.resolvePath("folder-black.svg"),
-    text: "Web Example",
-    isActive: false,
-    sortOrder: 30
-  });
+    function toggleWebOverlay() {
+        setWebOverlay(!webOverlay.isVisible());
+    }
 
-  button.clicked.connect(toggleWebOverlay);
-  
-  
-  //////////////////////////////
-  ////////// Cleanup ///////////
-  //////////////////////////////
-  
-  //Connect to the scriptEnding event to cleanup our objects
-  Script.scriptEnding.connect(scriptEnding);
-  function scriptEnding(){
-    button.clicked.disconnect(toggleWebOverlay);
-    tablet.removeButton(button);
-    webOverlay.webEventReceived.disconnect(webEvent);
-    webOverlay.close();
-  }
+    function openWebOverlay() {
+        setWebOverlay(true);
+    }
+
+    function closeOverlay() {
+        setWebOverlay(false);
+    }
+
+    function setWebOverlay(state) {
+        webOverlay.setVisible(state);
+        button.editProperties({ isActive: state });
+    }
+
+
+
+    //////////////////////////////
+    /////////// Button ///////////
+    //////////////////////////////
+
+    var tablet = Tablet.getTablet("com.highfidelity.interface.tablet.system");
+
+    var button = tablet.addButton({
+        icon: Script.resolvePath("folder-white.svg"),
+        activeIcon: Script.resolvePath("folder-black.svg"),
+        text: "Web Example",
+        isActive: false,
+        sortOrder: 30
+    });
+
+    button.clicked.connect(toggleWebOverlay);
+
+
+    //////////////////////////////
+    ////////// Cleanup ///////////
+    //////////////////////////////
+
+    //Connect to the scriptEnding event to cleanup our objects
+    Script.scriptEnding.connect(scriptEnding);
+
+    function scriptEnding() {
+        button.clicked.disconnect(toggleWebOverlay);
+        tablet.removeButton(button);
+        webOverlay.webEventReceived.disconnect(webEvent);
+        webOverlay.close();
+    }
 })();
